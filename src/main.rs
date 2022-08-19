bcae516faeb5b5c98a3ae763a5d1ecfaa0275293
Pietrek14 <dawidekpe@gmail.com>
2022-08-19 20:05:27 +0200

    initial commit (boilerplate)

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .run();
+}
